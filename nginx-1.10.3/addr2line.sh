d95c218697e47e46f884d3bda073426a9d8f5a92
liuhaijiang <liuhaijiang@huishoubao.com.cn>
2018-07-16 19:07:06 +0800

    打印函数深度

--- nginx-1.10.3/addr2line.sh
+++ nginx-1.10.3/addr2line.sh
@@ -4,22 +4,26 @@ if [ $# != 3 ]; then
     echo 'Usage: addr2line.sh executefile addressfile functionfile'  
     exit  
 fi;  
- 
+index=0
 cat $2 | while read line  
 do  
      if [ "$line" = 'Enter' ]; then  
              read line1  
              read line2  
 #           echo $line >> $3  
-             addr2line -e $1 -f $line1 -s >> $3  
-             echo "--->" >> $3  
+             addr2line -e $1 -f $line1 -s >> $3
+			 index=$((index+1))
+             str=$(printf "%-"$index"s-" "-")
+			 echo $index " ${str// /-}" >> $3  
              addr2line -e $1 -f $line2 -s | sed 's/^/    /' >> $3  
              echo >> $3  
      elif [ "$line" = 'Exit' ]; then  
              read line1  
              read line2  
              addr2line -e $1 -f $line2 -s | sed 's/^/    /' >> $3  
-             echo "<---" >> $3  
+			 index=$((index-1))
+             str=$(printf "%-"$index"s-" "-")
+			 echo  $index " ${str// /-}" >> $3   
              addr2line -e $1 -f $line1 -s >> $3  
 #           echo $line >> $3  
              echo >> $3  
