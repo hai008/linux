08a1f951a1621de582553cc80f73bf7d8c7ce4a6
liuhaijiang <liuhaijiang@huishoubao.com.cn>
2018-07-17 19:54:25 +0800

    树形结构打印调用关系

--- nginx-1.10.3/addr2line.sh
+++ nginx-1.10.3/addr2line.sh
@@ -10,22 +10,30 @@ do
      if [ "$line" = 'Enter' ]; then  
              read line1  
              read line2  
-#           echo $line >> $3  
-             addr2line -e $1 -f $line1 -s >> $3
-			 index=$((index+1))
+#           echo $line >> $3  	
+ 			 index=$((index+6))
+
              str=$(printf "%-"$index"s-" "-")
-			 echo $index " ${str// /-}" >> $3  
-             addr2line -e $1 -f $line2 -s | sed 's/^/    /' >> $3  
+			 text=$(echo -e ""  " ${str// /-}") 
+             text1="$text  $(addr2line -e $1 -f $line1 -s)"
+             text2="$text  $(addr2line -e $1 -f $line2 -s | sed 's/^/    /' )"
+			 #echo $text1 >> $3
+ 			 echo $text2 >> $3
+
              echo >> $3  
-     elif [ "$line" = 'Exit' ]; then  
+     elif [ "$line" = 'Exit' ]; then
+			 index=$((index-6))
+
+			 continue
              read line1  
              read line2  
              addr2line -e $1 -f $line2 -s | sed 's/^/    /' >> $3  
 			 index=$((index-1))
              str=$(printf "%-"$index"s-" "-")
-			 echo  $index " ${str// /-}" >> $3   
+			 echo -e "}out "$index " ${str// /-}/c" >> $3   
              addr2line -e $1 -f $line1 -s >> $3  
 #           echo $line >> $3  
              echo >> $3  
      fi;  
 done 
+#./addr2line.sh ./objs/nginx  /usr/local/nginx/sbin/mydebug.log mylog.log
