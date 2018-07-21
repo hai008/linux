a85b09e0049c1c79c881e7b99e45c137579d9397
liuhaijiang <liuhaijiang@huishoubao.com.cn>
2018-07-21 17:53:48 +0800

    更新，代码需要整理

--- nginx信号代码/upgrade.cpp
+++ nginx信号代码/upgrade.cpp
@@ -17,22 +17,21 @@
 
 #define LOCAL_PORT 6666　　　　　　   //本地服务端口
 #define MAX 5　　　　　　　　　　　　 //最大连接数量
-typedef pid_t       ngx_pid_t;
-#define  NGX_OK          0
-#define  NGX_ERROR      -1
-#define  NGX_AGAIN      -2
-#define  NGX_BUSY       -3
-#define  NGX_DONE       -4
-#define  NGX_DECLINED   -5
-#define  NGX_ABORT      -6
+typedef pid_t ngx_pid_t;
+#define NGX_OK 0
+#define NGX_ERROR -1
+#define NGX_AGAIN -2
+#define NGX_BUSY -3
+#define NGX_DONE -4
+#define NGX_DECLINED -5
+#define NGX_ABORT -6
 
 typedef intptr_t ngx_int_t;
 typedef uintptr_t ngx_uint_t;
 typedef intptr_t ngx_flag_t;
 
-#define ngx_memzero(buf, n)       (void) memset(buf, 0, n)  //ngx_memzero使用的是memset原型，memset使用汇编进行编写
-#define ngx_memset(buf, c, n)     (void) memset(buf, c, n)
-
+#define ngx_memzero(buf, n) (void)memset(buf, 0, n) //ngx_memzero使用的是memset原型，memset使用汇编进行编写
+#define ngx_memset(buf, c, n) (void)memset(buf, c, n)
 
 sig_atomic_t ngx_reap;
 sig_atomic_t ngx_sigio;
@@ -53,21 +52,19 @@ sig_atomic_t ngx_noaccept;
 ngx_uint_t ngx_noaccepting;
 ngx_uint_t ngx_restart;
 
-
 typedef struct
 {
   int signo;                  //信号的编号
-  char *signame;              //信号的字符串表现形式
-  char *name;                 //信号名称
+  const char *signame;        //信号的字符串表现形式
+  const char *name;           //信号名称
   void (*handler)(int signo); //信号处理函数
 } ngx_signal_t;
 
 #define ngx_signal_helper(n) SIG##n
 #define ngx_signal_value(n) ngx_signal_helper(n)
 
-
-#define ngx_value_helper(n)   #n
-#define ngx_value(n)          ngx_value_helper(n)
+#define ngx_value_helper(n) #n
+#define ngx_value(n) ngx_value_helper(n)
 
 #define ngx_random random
 
@@ -153,7 +150,7 @@ ngx_init_signals()
 void ngx_signal_handler(int signo)
 {
   printf("ngx_signal_handler\n");
-  char *action;
+  const char *action;
   switch (signo)
   {
 
@@ -193,6 +190,7 @@ void ngx_signal_handler(int signo)
 
   //热代码替换
   case ngx_signal_value(NGX_CHANGEBIN_SIGNAL):
+    printf("NGX_CHANGEBIN_SIGNAL\n");
     if (getppid() > 1 || ngx_new_binary > 0)
     {
 
@@ -211,6 +209,7 @@ void ngx_signal_handler(int signo)
                 */
       action = ", ignoring";
       //ignore = 1;
+      ngx_change_binary = 1;
       break;
     }
 
@@ -234,139 +233,196 @@ void ngx_signal_handler(int signo)
 }
 #define MAXLINE 32
 #define error_exit(msg) \
-    do { perror(msg); exit(EXIT_FAILURE); } while (0)
-int main()
+  do                    \
+  {                     \
+    perror(msg);        \
+    exit(EXIT_FAILURE); \
+  } while (0)
+int main(int argc, char *argv[])
 {
+  printf("bg main\n");
+  printf("%d\n", getpid());
   ngx_init_signals();
-   struct epoll_event event;   // 告诉内核要监听什么事件  
-    struct epoll_event wait_event; //内核监听完的结果
-	
-	//1.创建tcp监听套接字
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	
-	//2.绑定sockfd
-	struct sockaddr_in my_addr;
-	bzero(&my_addr, sizeof(my_addr));
-	my_addr.sin_family = AF_INET;
-	my_addr.sin_port = htons(8001);
-	my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	bind(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr));
-	
-	//3.监听listen
-	listen(sockfd, 10);
-	 
-	//4.epoll相应参数准备
-	int fd[OPEN_MAX];
-	int i = 0, maxi = 0;
-	memset(fd,-1, sizeof(fd));
-	fd[0] = sockfd;
-	
-	int epfd = epoll_create(10); // 创建一个 epoll 的句柄，参数要大于 0， 没有太大意义  
-    if( -1 == epfd ){  
-        perror ("epoll_create");  
-        return -1;  
-    }  
-      
-    event.data.fd = sockfd;     //监听套接字  
-    event.events = EPOLLIN; // 表示对应的文件描述符可以读
-	
-	//5.事件注册函数，将监听套接字描述符 sockfd 加入监听事件  
-    int ret = epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &event);  
-    if(-1 == ret){  
-        perror("epoll_ctl");  
-        return -1;  
-    } 
-	
-	//6.对已连接的客户端的数据处理
-	while(1)
-	{
-		// 监视并等待多个文件（标准输入，udp套接字）描述符的属性变化（是否可读）  
-        // 没有属性变化，这个函数会阻塞，直到有变化才往下执行，这里没有设置超时 
-    printf("bf epoll_wait\n");  
-    ret = epoll_wait(epfd, &wait_event, maxi+1, -1);
-    //printf("signal %d!", ret);
-		if(ret == -1 && errno == EINTR)
-    {
-        printf("signal!\n");
-        printf("signal!\n");
-        continue;
-    }
-		//6.1监测sockfd(监听套接字)是否存在连接
-		if(( sockfd == wait_event.data.fd )   
-            && ( EPOLLIN == wait_event.events & EPOLLIN ) )
-		{
-			struct sockaddr_in cli_addr;
-			socklen_t  clilen = sizeof(cli_addr);
-			
-			//6.1.1 从tcp完成连接中提取客户端
-			int connfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-			
-			//6.1.2 将提取到的connfd放入fd数组中，以便下面轮询客户端套接字
-			for(i=1; i<OPEN_MAX; i++)
-			{
-				if(fd[i] < 0)
-				{
-					fd[i] = connfd;
-					event.data.fd = connfd; //监听套接字  
-					event.events = EPOLLIN; // 表示对应的文件描述符可以读
-					
-					//6.1.3.事件注册函数，将监听套接字描述符 connfd 加入监听事件  
-					ret = epoll_ctl(epfd, EPOLL_CTL_ADD, connfd, &event);  
-					if(-1 == ret){  
-						perror("epoll_ctl");  
-						return -1;  
-					} 
-					
-					break;
-				}
-			}
-			
-			//6.1.4 maxi更新
-			if(i > maxi)
-				maxi = i;
-				
-			//6.1.5 如果没有就绪的描述符，就继续epoll监测，否则继续向下看
-			if(--ret <= 0)
-				continue;
-		}
-		
-		//6.2继续响应就绪的描述符
-		for(i=1; i<=maxi; i++)
-		{
-			if(fd[i] < 0)
-				continue;
-			
-			if(( fd[i] == wait_event.data.fd )   
-            && ( EPOLLIN == wait_event.events & (EPOLLIN|EPOLLERR) ))
-			{
-				int len = 0;
-				char buf[128] = "";
-				
-				//6.2.1接受客户端数据
-				if((len = recv(fd[i], buf, sizeof(buf), 0)) < 0)
-				{
-					if(errno == ECONNRESET)//tcp连接超时、RST
-					{
-						close(fd[i]);
-						fd[i] = -1;
-					}
-					else
-						perror("read error:");
-				}
-				else if(len == 0)//客户端关闭连接
-				{
-					close(fd[i]);
-					fd[i] = -1;
-				}
-				else//正常接收到服务器的数据
-					send(fd[i], buf, len, 0);
-				
-				//6.2.2所有的就绪描述符处理完了，就退出当前的for循环，继续poll监测
-				if(--ret <= 0)
-					break;
-			}
-		}
-	}
+  struct epoll_event event;      // 告诉内核要监听什么事件
+  struct epoll_event wait_event; //内核监听完的结果
 
-    return 0;
+  //1.创建tcp监听套接字
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  //2.绑定sockfd
+  struct sockaddr_in my_addr;
+  bzero(&my_addr, sizeof(my_addr));
+  my_addr.sin_family = AF_INET;
+  my_addr.sin_port = htons(8001);
+  my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  if(argc == 1)
+  {
+    bind(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr));
+
+  //3.监听listen
+    listen(sockfd, 10);
+   
+  }
+  else
+  {
+    printf("%d", argc);
+    printf(argv[0]);
+    printf(argv[1]);
+    printf(argv[2]);
+     sockfd = atoi(argv[1]);
+  }
+  //4.epoll相应参数准备
+  int fd[OPEN_MAX];
+  int i = 0, maxi = 0;
+  memset(fd, -1, sizeof(fd));
+  fd[0] = sockfd;
+
+  int epfd = epoll_create(10); // 创建一个 epoll 的句柄，参数要大于 0， 没有太大意义
+  if (-1 == epfd)
+  {
+    perror("epoll_create");
+    return -1;
+  }
+  printf("socked %d \n", sockfd);
+  event.data.fd = sockfd; //监听套接字
+  event.events = EPOLLIN; // 表示对应的文件描述符可以读
+
+  //5.事件注册函数，将监听套接字描述符 sockfd 加入监听事件
+  int ret = epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &event);
+  if (-1 == ret)
+  {
+    perror("epoll_ctl");
+    return -1;
+  }
+
+  //6.对已连接的客户端的数据处理
+  while (1)
+  {
+    // 监视并等待多个文件（标准输入，udp套接字）描述符的属性变化（是否可读）
+    // 没有属性变化，这个函数会阻塞，直到有变化才往下执行，这里没有设置超时
+      printf("bf epoll_wait\n");
+      ret = epoll_wait(epfd, &wait_event, maxi + 1, -1);
+      //printf("signal %d!", ret);
+      if (ret == -1 && errno == EINTR)
+      {
+        printf("signal!\n");
+        printf("signal!\n");
+        if (ngx_change_binary)
+        {
+          ngx_pid_t  pid;
+          pid = fork();
+          if(pid == 0)
+          {
+            printf("new binary\n");
+            char csockfd[64];
+            ngx_change_binary = 0;
+            snprintf(csockfd, 64, "%d", sockfd);
+            char * newargv[ ]={"a.out",csockfd,"/etc/passwd",(char *)0};
+            char * envp[ ]={"PATH=/bin",0};
+            execve("/home/l/upgrate/a.out",newargv,envp);
+
+          }
+          sleep(1);
+         
+        }
+        
+        //continue;
+      }
+              printf("ngx_change_binary :%d", ngx_change_binary);
+
+      if(ngx_change_binary)
+      {
+        bool bexit = true;
+        for (i = 1; i < OPEN_MAX; i++)
+        {
+          if (fd[i] > 0 )
+          {
+             bexit = false;
+          }
+        }
+        if(bexit)
+        {
+          exit(0);
+        }
+      }
+      //6.1监测sockfd(监听套接字)是否存在连接
+      if ((sockfd == wait_event.data.fd) && (EPOLLIN == wait_event.events & EPOLLIN))
+      {
+        struct sockaddr_in cli_addr;
+        socklen_t clilen = sizeof(cli_addr);
+
+        //6.1.1 从tcp完成连接中提取客户端
+        int connfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+
+        //6.1.2 将提取到的connfd放入fd数组中，以便下面轮询客户端套接字
+        for (i = 1; i < OPEN_MAX; i++)
+        {
+          if (fd[i] < 0)
+          {
+            fd[i] = connfd;
+            event.data.fd = connfd; //监听套接字
+            event.events = EPOLLIN; // 表示对应的文件描述符可以读
+
+            //6.1.3.事件注册函数，将监听套接字描述符 connfd 加入监听事件
+            if(ngx_change_binary == 0)
+              ret = epoll_ctl(epfd, EPOLL_CTL_ADD, connfd, &event);
+            if (-1 == ret)
+            {
+              perror("epoll_ctl");
+              return -1;
+            }
+
+            break;
+          }
+        }
+
+        //6.1.4 maxi更新
+        if (i > maxi)
+          maxi = i;
+
+        //6.1.5 如果没有就绪的描述符，就继续epoll监测，否则继续向下看
+        if (--ret <= 0)
+          continue;
+      }
+
+      //6.2继续响应就绪的描述符
+      for (i = 1; i <= maxi; i++)
+      {
+        if (fd[i] < 0)
+          continue;
+
+        if ((fd[i] == wait_event.data.fd) && (EPOLLIN == wait_event.events & (EPOLLIN | EPOLLERR)))
+        {
+          int len = 0;
+          char buf[128] = "";
+
+          //6.2.1接受客户端数据
+          if ((len = recv(fd[i], buf, sizeof(buf), 0)) < 0)
+          {
+            buf[127] = '\0';
+            printf(buf);
+            if (errno == ECONNRESET) //tcp连接超时、RST
+            {
+              close(fd[i]);
+              fd[i] = -1;
+            }
+            else
+              perror("read error:");
+          }
+          else if (len == 0) //客户端关闭连接
+          {
+            close(fd[i]);
+            fd[i] = -1;
+          }
+          else //正常接收到服务器的数据
+            send(fd[i], buf, len, 0);
+
+          //6.2.2所有的就绪描述符处理完了，就退出当前的for循环，继续poll监测
+          if (--ret <= 0)
+            break;
+        }
+      }
+  }
+
+  return 0;
 }
